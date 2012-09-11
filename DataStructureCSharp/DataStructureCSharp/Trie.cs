98832b0868bc0d87727552c6876c771ae903493d
unknown <viet.hoang00@gmail.com>
2012-09-10 20:51:14 -0700

    upload Trie, TrieNode

---
+++ DataStructureCSharp/DataStructureCSharp/Trie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructureCSharp
+{
+    class Trie
+    {
+        private TrieNode root;
+        public Trie() {
+            root = new TrieNode(' ');
+        }
+
+        public void insert(string num) {
+            TrieNode here = root;
+            foreach (char c in num){
+                if (here.getChild((int)c) == null){
+                    TrieNode newChild = new TrieNode(c);
+                    here.setChild((int)c, c);
+                }
+                here = here.getChild((int)c);
+            }
+            here.getChild((int)num[num.Length-1]).setEnd(true);//set isWordEnd = true
+        }//end insert
+    }
+}
---
+++ DataStructureCSharp/DataStructureCSharp/TrieNode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructureCSharp
+{
+    class TrieNode
+    {
+        private char letter;
+        private bool isWordEnd;
+        private TrieNode[] theChildren;
+        
+        public TrieNode() { 
+            letter = ' ';
+            isWordEnd = false;
+            theChildren = new TrieNode[10];
+        }
+
+        public TrieNode(char l) {
+            letter = l;
+            isWordEnd = false;
+            theChildren = new TrieNode[10];
+        }
+
+        public Boolean getEnds() {
+            return isWordEnd;
+        }
+
+        public TrieNode getChild(int i) { 
+            return theChildren[i];
+        }
+
+        public void setEnd(bool e) {
+            isWordEnd = e;
+        }
+
+        public void setChild(int i, char l) {
+            theChildren[i] = new TrieNode(l);
+        }
+
+    }//end TrieNode
+}//end namespace
